77af4d1de42c0421d6ab2ef6efa856d1fb8e8225
Juan Pablo Arias <juanpabloarias803@gmail.com>
2024-07-25 06:47:09 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/navbar/navbar'
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <Navbar/>
+        <h1>Hola mundo react</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
